ab4b98a60c5df1f79eebcee6fbba0ff7fd7315d3
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-03-07 12:32:16 +0900

    上からキューブが降ってくるように

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,19 @@ import {
   Mesh,
   MeshBuilder,
   FreeCamera,
+  HavokPlugin,
+  PhysicsAggregate,
+  PhysicsShapeType,
+  StandardMaterial,
+  Color3,
 } from "@babylonjs/core";
+import HavokPhysics from "@babylonjs/havok";
+// @ts-ignore
+import havokWasmUrl from "../assets/HavokPhysics.wasm?url";
+
+const havok = await HavokPhysics({
+  locateFile: () => havokWasmUrl,
+});
 
 class App {
   px: number = 0;
@@ -27,9 +39,12 @@ class App {
     const engine = new Engine(canvas, true);
     const scene = new Scene(engine);
 
+    const havokPlugin = new HavokPlugin(true, havok);
+    scene.enablePhysics(new Vector3(0, -9.8, 0), havokPlugin);
+
     const camera: FreeCamera = new FreeCamera(
       "camera",
-      new Vector3(0, 5, -10),
+      new Vector3(5, 5, -10),
       scene
     );
     camera.setTarget(Vector3.Zero());
@@ -39,8 +54,13 @@ class App {
       new Vector3(1, 1, 0),
       scene
     );
+
     const cube: Mesh = MeshBuilder.CreateBox("box", { size: 1 }, scene);
-    cube.position = new Vector3(0, 2, 0);
+    const cubeMaterial = new StandardMaterial("boxMaterial", scene);
+    cubeMaterial.diffuseColor = new Color3(0.2, 0.4, 0.8);
+    cube.material = cubeMaterial;
+    cube.position = new Vector3(0, 10, 0);
+
     const ground: Mesh = MeshBuilder.CreateGround(
       "ground",
       { width: 10, height: 10 },
@@ -57,7 +77,21 @@ class App {
       ground.rotate(new Vector3(0, 0, 1), ((this.py - y) / 180) * Math.PI);
       this.px = x;
       this.py = y;
+      console.log(this.px, this.py);
     });
+
+    const cubeAggregate = new PhysicsAggregate(
+      cube,
+      PhysicsShapeType.BOX,
+      { mass: 1, restitution: 0.2 },
+      scene
+    );
+    const groundAggregate = new PhysicsAggregate(
+      ground,
+      PhysicsShapeType.BOX,
+      { mass: 0 },
+      scene
+    );
 
     // hide/show the Inspector
     window.addEventListener("keydown", (ev) => {
