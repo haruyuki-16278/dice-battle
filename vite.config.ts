b9ce1e0be32b3ecd23083e294965ee095ad1a6c1
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-03-09 12:03:22 +0900

    baseパスは/終わりである方が好ましいので修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('vite').UserConfig} */
 export default {
-  base: process.env.GITHUB_PAGES ? "/dice-battle" : "/",
+  base: process.env.GITHUB_PAGES ? "/dice-battle/" : "/",
   build: {
     target: "ESNext",
   },
