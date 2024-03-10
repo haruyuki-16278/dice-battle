76c8b75c6d64c6d8cf220daa894eab10b2820344
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-03-11 00:02:04 +0900

    pwa有効化

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-/** @type {import('vite').UserConfig} */
-export default {
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
   base: process.env.GITHUB_PAGES ? "/dice-battle/" : "/",
   build: {
     target: "ESNext",
   },
-};
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      injectRegister: "auto",
+      manifest: {
+        name: "dice-battle",
+        short_name: "dice-battle",
+        description: "shake device -> roll dice",
+        theme_color: "#000",
+        background_color: "#FFF",
+        icons: [
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
