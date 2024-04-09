e6781efcd19a4df9a7928975b987d8694302c4f8
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-04-09 22:30:08 +0900

    ルーム削除APIを実装

---
+++ api/room/delete/index.ts
@@ -0,0 +1,64 @@
+import {
+  APIGatewayProxyResultV2,
+  Context,
+} from "https://deno.land/x/lambda@1.41.1/mod.ts";
+import { DynamoDBClient } from "https://esm.sh/@aws-sdk/client-dynamodb@3.538.0";
+import {
+  DynamoDBDocumentClient,
+  DeleteCommand,
+} from "https://esm.sh/@aws-sdk/lib-dynamodb@3.538.0";
+
+const key = Deno.env.get("ACCESS");
+const secret = Deno.env.get("SECRET");
+
+if (!key || !secret) {
+  console.error("credentials unset");
+  Deno.exit(1);
+}
+
+const ddbClient = new DynamoDBClient({
+  region: Deno.env.get("AWS_REGION"),
+  credentials: {
+    accessKeyId: key,
+    secretAccessKey: secret,
+  },
+});
+
+const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
+const tableName = "dice-battle";
+
+// deno-lint-ignore require-await
+export async function handler(
+  data: { roomID: string },
+  _context: Context
+): Promise<APIGatewayProxyResultV2> {
+  if (data.roomID) {
+    [];
+    const result = await ddbDocClient.send(
+      new DeleteCommand({
+        TableName: tableName,
+        Key: {
+          id: data.roomID,
+        },
+      })
+    );
+    console.log(result);
+
+    return {
+      statusCode: 200,
+      headers: { "content-type": "application/json;charset=utf8" },
+      body: JSON.stringify({
+        code: "S-001",
+        message: "succeeded",
+      }),
+    };
+  }
+  return {
+    statusCode: 200,
+    headers: { "content-type": "application/json;charset=utf8" },
+    body: JSON.stringify({
+      code: "E-001",
+      message: "invalid data",
+    }),
+  };
+}
